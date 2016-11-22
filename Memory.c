b70fe058b6a2fc2d9db8b5766628905273c680fa
madzombie <mathieubelanger14@gmail.com>
2016-11-22 12:09:59 -0500

    Adding basic structure for memory management. Memory is our list of Blocs and MemoryManager is the interface used by a program to receive memory.

---
+++ Memory.c
@@ -0,0 +1,4 @@
+//
+// Created by mathieu on 11/22/16.
+//
+
---
+++ Memory.h
@@ -0,0 +1,8 @@
+//
+// Created by mathieu on 11/22/16.
+//
+
+#ifndef LOG710_LAB3_MEMORY_H
+#define LOG710_LAB3_MEMORY_H
+
+#endif //LOG710_LAB3_MEMORY_H
